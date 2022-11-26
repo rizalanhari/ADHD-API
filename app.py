c9f5b72652f4d46f277db3d93795353d337c2c70
rizalanhari <rizalanhari6@gmail.com>
2022-11-26 17:24:51 +0700

    app

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import pandas as pd
 app = Flask(__name__)
 
 dataTrain = pd.read_csv("https://raw.githubusercontent.com/rizalanhari/ADHD-API/main/DataTrainApi.csv")
+dataTest = pd.read_csv("https://raw.githubusercontent.com/rizalanhari/ADHD-API/main/DataTestApi.csv")
+question = pd.read_csv("https://raw.githubusercontent.com/rizalanhari/ADHD-API/main/question.csv")
 
 @app.route('/')
 def getHello():
@@ -12,6 +14,15 @@ def getHello():
 @app.route('/datatrain')
 def getDataTrain():
     return dataTrain.to_json(orient='records')
+
+@app.route('/datatest')
+def getDataTest():
+    return dataTest.to_json(orient='records')
+
+
+@app.route('/question')
+def getDataQuestion():
+    return question.to_json(orient='records')
 
 if __name__ == '__main__':
     app.run()
