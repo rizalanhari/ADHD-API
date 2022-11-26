c5419c51b32c311e88deee51a4b328e905c66703
rizalanhari <rizalanhari6@gmail.com>
2022-11-26 14:25:09 +0700

    up datatrain

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, jsonify, request
+import pandas as pd
+
+dataTrain = pd.read_csv("dataTrainAPI.csv")
 
 app = Flask(__name__)
 
 @app.route('/')
 def getHello():
     return "Hello, API ADHD"
+
+@app.route('/datatrain')
+def getDataTrain():
+    return dataTrain.to_json(orient='records')
 
 if __name__ == '__main__':
     app.run()
