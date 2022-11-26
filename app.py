f613ca214b99d255f552acf94c60a1510014aa84
rizalanhari <rizalanhari6@gmail.com>
2022-11-26 17:51:51 +0700

    3

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 import pandas as pd
 import json
+from BackpropData import *
 
 app = Flask(__name__)
 
@@ -24,6 +25,38 @@ def getDataTest():
 @app.route('/question')
 def getDataQuestion():
     return question.to_json(orient='records')
+
+@app.route('/predictA')
+def predictA():
+    train = int(request.args.get('train'))
+    test = int(request.args.get('test'))
+    lrate = float(request.args.get('lrate'))
+    neuronh = int(request.args.get('neuronh'))
+    output, true, acc = predictAdmin(train, test, lrate, neuronh)
+    output2 = np.append(output, acc)
+    true2 = np.append(true, acc)
+    heading = []
+    cols = len(output)
+    for i in range(cols):
+        heading.append('data %d' % (i+1))
+    heading.append("acc")
+    result = np.vstack((output2, true2))
+    df = pd.DataFrame(result, columns=heading)
+    return df.to_json(orient='records')
+
+
+@app.route('/predict')
+def predict():
+    data = []
+    col = []
+    for x in range(45):
+        temp_data = int(request.args.get('data'+str(x)))
+        print(temp_data)
+        col.append(temp_data)
+    data.append(col)
+    hasil = predictUser(data)
+    json_hasil = json.dumps({'Hasil': int(hasil[0])})
+    return json_hasil
 
 if __name__ == '__main__':
     app.run()
